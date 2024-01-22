a9cf1c75eea7385e0e3474ac3b16b3f68b8d50d6
Junichiro Doi <jdoi.work@gmail.com>
2024-01-22 20:42:44 +0900

    Add decodePoland

--- Chap1/TenPuzzle/TenPuzzle.hs
+++ Chap1/TenPuzzle/TenPuzzle.hs
@@ -4,7 +4,8 @@ import Data.Char (isNumber)
 
 main = do
   print $ calcPoland "612+*8-"
-  
+  print $ decodePoland "612+*8-"
+
 
 calcPoland :: String -> Maybe Double
 calcPoland s = go s []
@@ -22,3 +23,30 @@ calcPoland s = go s []
                | otherwise  = Nothing
     apply :: Double -> Double -> [Double] -> (Double -> Double -> Double) -> [Double]
     apply x y z op = (op y x):z
+
+decodePoland :: String -> Maybe String
+decodePoland s = go s []
+  where
+    go :: String -> [String] -> Maybe String
+    go [] [x] = Just $ reverse x
+    go [] _   = Nothing
+    go (c:s) x       | isNumber c = go s ([c]:x)
+    go (c:s) (a:b:x) | isOp     c = go s ((applyOp a b c):x)
+                     | otherwise  = Nothing
+    applyOp a b '*' = (enclose a) ++ " * " ++ (enclose b)
+    applyOp a b '/' = (enclose a) ++ " / " ++ (enclose b)
+    applyOp a b '+' = a ++ " + " ++ b
+    applyOp a b '-' = a ++ " - " ++ b
+    
+    enclose :: String -> String
+    enclose [c] = [c]
+    enclose s = ")" ++ s ++ "("
+
+
+
+isOp :: Char -> Bool
+isOp '+' = True
+isOp '-' = True
+isOp '*' = True
+isOp '/' = True
+isOp _   = False
